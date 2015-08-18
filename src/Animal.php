79ceaa1cee8d93a0a40f05ac5e62d17b07aeb4fa
Phillip Shannon & christopher calascione <student@epicodus.com>
2015-08-18 16:03:52 -0700

    Add save and getAll methods with tests.

--- src/Animal.php
+++ src/Animal.php
@@ -47,5 +47,45 @@
         {
             $this->name = $new_name;
         }
+        function setGender($new_gender)
+        {
+            $this->gender = $new_gender;
+        }
+        function setBreed($new_breed)
+        {
+            $this->breed = $new_breed;
+        }
+        function setDateOfAdmittance($new_doa)
+        {
+            $this->date_of_admittance = $new_doa;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO animals (name, gender, breed, date_of_admittance, animal_type_id) VALUES ('{$this->getName()}', '{$this->getGender()}', '{$this->getBreed()}', '{$this->getDateOfAdmittance()}', {$this->getAnimalTypeId()});");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_animals = $GLOBALS['DB']->query("SELECT * FROM animals;");
+            $animals = array();
+            foreach($returned_animals as $animal) {
+                $name = $animal['name'];
+                $gender = $animal['gender'];
+                $breed = $animal['breed'];
+                $date_of_admittance = $animal["date_of_admittance"];
+                $animal_type_id = $animal["animal_type_id"];
+                $id = $animal['id'];
+                $new_animal = new Animal($name, $gender, $breed, $date_of_admittance, $animal_type_id, $id);
+                array_push($animals, $new_animal);
+            }
+            return $animals;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM animals;");
+        }
     }
 ?>
---
+++ test/AnimalTest.php
@@ -0,0 +1,77 @@
+<?php
+
+     /**
+     * @backupGlobals disabled
+     * @backupStaticAttributes disabled
+     */
+
+     require_once "src/Animal.php";
+     require_once "src/AnimalType.php";
+
+
+     $server = 'mysql:host=localhost;dbname=shelter_test';
+     $username = 'root';
+     $password = 'root';
+     $DB = new PDO($server, $username, $password);
+
+     class AnimalTest extends PHPUnit_Framework_TestCase
+     {
+         protected function tearDown()
+         {
+             Animal::deleteAll();
+         }
+
+         function test_save()
+         {
+
+             //Arrange
+             $name = "Henry";
+             $gender = "Male";
+             $breed = "Beagle";
+             $date_of_admittance = "2015-09-01";
+             $animal_type_id = 1;
+             $id = null;
+             $test_Animal = new Animal($name, $gender, $breed, $date_of_admittance, $animal_type_id, $id);
+
+             //Act
+             $test_Animal->save();
+
+             //Assert
+             $result = Animal::getAll();
+             $this->assertEquals($test_Animal, $result[0]);
+
+         }
+
+         function test_getAll()
+         {
+             //Arrange
+             $name = "Henry";
+             $gender = "Male";
+             $breed = "Beagle";
+             $date_of_admittance = "2015-09-01";
+             $animal_type_id = 1;
+             $id = null;
+             $test_Animal = new Animal($name, $gender, $breed, $date_of_admittance, $animal_type_id, $id);
+             $test_Animal->save();
+             $name2 = "Sparky";
+             $gender2 = "Female";
+             $breed2 = "Pitbull";
+             $date_of_admittance2 = "2013-04-01";
+             $animal_type_id2 = 1;
+             $id2 = null;
+             $test_Animal2 = new Animal($name2, $gender2, $breed2, $date_of_admittance2, $animal_type_id2, $id2);
+             $test_Animal2->save();
+
+             //Act
+             $result = Animal::getAll();
+
+             //Assert
+             $this->assertEquals([$test_Animal, $test_Animal2], $result);
+         }
+     }
+
+
+
+
+
+ ?>
